91907d97644c69ad4a95d350d6287fbf1b9c9a4c
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-10-13 14:24:22 +0530

    destructured project

---
+++ home/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+
+class Item(models.Model):
+    item_image = models.ImageField(upload_to='items/')
+    item_name = models.CharField(max_length=100)
+    item_description = models.TextField()
+    item_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.item_name
+    
+class Menu_Board_items(models.Model):
+    item_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.item_name
+
+class LunchMenu(models.Model):
+    items = models.ManyToManyField(Item, related_name='lunch_menus')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Lunch Menu {self.id}"
+
+class DinnerMenu(models.Model):
+    items = models.ManyToManyField(Item, related_name='dinner_menus')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Dinner Menu {self.id}"
+    
+class DeliciousMenu(models.Model):
+    items = models.ManyToManyField(Item, related_name='delicious_menus')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Dinner Menu {self.id}"
+
+class TodayLunchMenu(models.Model):
+    options = (
+        ("veg","veg"),
+        ("non_veg","non_veg"),
+    )
+    item_category=models.CharField(max_length=100,choices=options)
+    items = models.ManyToManyField(Menu_Board_items, related_name='today_lunch_menu')
+    price = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Menu for {self.date}"
+    
+class TodayDinnerMenu(models.Model):
+    options = (
+        ("veg","veg"),
+        ("non_veg","non_veg"),
+    )
+    item_category=models.CharField(max_length=100,choices=options)
+    items = models.ManyToManyField(Menu_Board_items, related_name='today_dinner_menu')
+    price = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Menu for {self.date}"
+
+class Scanner(models.Model):
+    scanner_image = models.ImageField(upload_to='scanner/')
+
+    def __str__(self):
+        return "Scanner"
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=12, unique=True, blank=True, null=True)
+    payment_id = models.CharField(max_length=100, unique=True, blank=True, null=True)
+    pdf_invoice = models.FileField(upload_to='invoices/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    customer_email = models.EmailField(blank=True, null=True)
+    payment_status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('paid', 'Paid')], default='pending')
+    payment_mode = models.CharField(max_length=10, choices=[('online', 'Online'), ('cash', 'Cash')], default='online')
+
+    def __str__(self):
+        return f"Order {self.order_id} - Payment {self.payment_id}"
